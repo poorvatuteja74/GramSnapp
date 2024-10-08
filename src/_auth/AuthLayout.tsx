36506876e7d55870c359499d0f1a2c284a02fa38
poorvatuteja74 <168651056+poorvatuteja74@users.noreply.github.com>
2024-10-08 19:42:28 -0400

    sign up page image added

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,6 +1,8 @@
 import { Outlet, Navigate } from 'react-router-dom';
 import React from 'react';
 
+
+
 const AuthLayout = () => {
   const isAuthenticated = false;
   return (
@@ -8,9 +10,17 @@ const AuthLayout = () => {
       {isAuthenticated ? (
         <Navigate to='/' />
       ) : (
-        <section>
+        <>
+        <section className="flex flex-1 justify-center items-center flex-col py-10">
           <Outlet />
         </section>
+        <img 
+        src='/assets/images/side-img.svg'
+        alt='logo'
+        className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'
+
+        />
+        </>
       )}
     </>
   );
